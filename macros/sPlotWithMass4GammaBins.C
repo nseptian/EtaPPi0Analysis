8a7189aa19bfa17ca49650bf7c485618d04c736b
Nizar Septian <nizar47@gmail.com>
2024-05-24 16:04:27 -0400

    Add drawer with weights to sPlotWithMass4GammaBins.C

--- macros/sPlotWithMass4GammaBins.C
+++ macros/sPlotWithMass4GammaBins.C
@@ -14,17 +14,18 @@ sPlot RF;
 
 void DoFit();
 void DrawWithWeights(TString weightFileName);
-void DrawWithoutWeights();
+void DrawWithoutWeights(TString inFileName, TString weightFileName, Int_t iVar=0);
 void my_style();
 TH1F* GetUnweightedEtaPrimeMassHistogram(TString inFileName, Int_t iBin);
 TPad* GetsPlotFitPad(TString inFileName, TString binVarName, TString sPlotVarName, Int_t iBin);
 void DrawsPlotFit();
 
 void sPlotWithMass4GammaBins(){
-    // DoFit();
     // my_style();
+    // DoFit();
     // DrawWithoutWeights();
     // DrawsPlotFit();
+    DrawWithWeights(Form("%s%s",dataDir.Data(),rootFile.Data()));
 }
 
 void DoFit(){
@@ -134,6 +135,172 @@ TPad* GetsPlotFitPad(TString inFileName, TString binVarName, TString sPlotVarNam
     // pad->SaveAs(Form("%s.pdf",inFileName.Data()));
 
     return pad;
+}
+
+void DrawWithWeights(TString inFileName){
+    my_style();
+
+    TChain chain("ntFSGlueX_100_4000000");
+
+    cout << "Adding file " << inFileName << " to the chain" << endl;
+    chain.Add(inFileName);
+
+    Double_t UID;
+    Double_t Mass2Gamma[6];
+    Double_t Mass4Gamma;
+    Double_t MassPi0Gamma1;
+    Double_t MassPi0Gamma2;
+    TLorentzVector P4_Gamma_Measured[4];
+    TLorentzVector P4_Gamma_KinFit[4];
+    TLorentzVector P4_RecoilProton;
+    TLorentzVector P4_TargetProton = TLorentzVector(0,0,0,0.938272);
+
+    chain.SetBranchAddress("UID",&UID);
+    chain.SetBranchAddress("MassGamma2Gamma3",&Mass2Gamma[0]);
+    chain.SetBranchAddress("MassGamma2Gamma4",&Mass2Gamma[1]);
+    chain.SetBranchAddress("MassGamma2Gamma5",&Mass2Gamma[2]);
+    chain.SetBranchAddress("MassGamma3Gamma4",&Mass2Gamma[3]);
+    chain.SetBranchAddress("MassGamma3Gamma5",&Mass2Gamma[4]);
+    chain.SetBranchAddress("MassGamma4Gamma5",&Mass2Gamma[5]);
+    chain.SetBranchAddress("Mass4Gamma",&Mass4Gamma);
+    chain.SetBranchAddress("MassPi0Gamma1",&MassPi0Gamma1);
+    chain.SetBranchAddress("MassPi0Gamma2",&MassPi0Gamma2);
+
+    TH1F *h1_MassGamma2Gamma3 = new TH1F("h1_MassGamma2Gamma3","m(#gamma_{2}#gamma_{3})",40,0.85,1.05);
+    TH1F *h1_MassGamma2Gamma4 = new TH1F("h1_MassGamma2Gamma4","m(#gamma_{2}#gamma_{4})",200,0,1.0);
+    TH1F *h1_MassGamma2Gamma5 = new TH1F("h1_MassGamma2Gamma5","m(#gamma_{2}#gamma_{5})",200,0,1.0);
+    TH1F *h1_MassGamma3Gamma4 = new TH1F("h1_MassGamma3Gamma4","m(#gamma_{3}#gamma_{4})",200,0,1.0);
+    TH1F *h1_MassGamma3Gamma5 = new TH1F("h1_MassGamma3Gamma5","m(#gamma_{3}#gamma_{5})",200,0,1.0);
+    TH1F *h1_MassGamma4Gamma5 = new TH1F("h1_MassGamma4Gamma5","m(#gamma_{4}#gamma_{5})",40,0.0,0.2);
+    TH1F *h1_Mass4Gamma_Signal = new TH1F("h1_Mass4Gamma_Signal","m_{4#gamma}",50,1.0,3.0);
+    TH1F *h1_Mass4Gamma_BG = new TH1F("h1_Mass4Gamma_BG","m_{4#gamma}",50,1.0,3.0);
+    TH1F *h1_MassPi0Gamma1_Signal = new TH1F("h1_MassPi0Gamma1_Signal","m(#pi^{0}#gamma_{2})",100,0.0,3.0);
+    TH1F *h1_MassPi0Gamma2_Signal = new TH1F("h1_MassPi0Gamma2_Signal","m(#pi^{0}#gamma_{3})",100,0.0,3.0);
+    TH1F *h1_MassPi0Gamma1_BG = new TH1F("h1_MassPi0Gamma1_BG","m(#pi^{0}#gamma_{2})",100,0.0,3.0);
+    TH1F *h1_MassPi0Gamma2_BG = new TH1F("h1_MassPi0Gamma2_BG","m(#pi^{0}#gamma_{3})",100,0.0,3.0);
+
+    cout << "Processing " << chain.GetEntries() << " entries" << endl;
+    cout << "Filling histograms..." << endl;
+
+    Weights *wts[NMass4GammaBins];
+
+    for (Int_t iBin=0;iBin<NMass4GammaBins;iBin++){
+        Double_t centerBin = Mass4GammaLowLim + (Mass4GammaUpLim - Mass4GammaLowLim)/NMass4GammaBins*(iBin+0.5);
+        TString weightFileName = Form("%sWeightsMass4Gamma%1.2f_.root",outDir.Data(),centerBin);
+        wts[iBin] = new Weights("Weights");
+        cout << "Loading weights from " << weightFileName << endl;
+        wts[iBin]->LoadSaved(weightFileName,"HSsWeights");
+        for (Int_t iEntry=0;iEntry<chain.GetEntries();iEntry++){
+            chain.GetEntry(iEntry);
+            if(wts[iBin]->GetEntryBinarySearch(UID)){
+                Double_t sigweight = wts[iBin]->GetWeight("Signal");
+                Double_t bkgweight = wts[iBin]->GetWeight("BG");
+
+                // cout << UID << " " << weight << endl;
+                h1_MassGamma2Gamma3->Fill(Mass2Gamma[0],sigweight);
+                h1_MassGamma2Gamma4->Fill(Mass2Gamma[1],sigweight);
+                h1_MassGamma2Gamma5->Fill(Mass2Gamma[2],sigweight);
+                h1_MassGamma3Gamma4->Fill(Mass2Gamma[3],sigweight);
+                h1_MassGamma3Gamma5->Fill(Mass2Gamma[4],sigweight);
+                h1_MassGamma4Gamma5->Fill(Mass2Gamma[5],sigweight);
+                h1_Mass4Gamma_Signal->Fill(Mass4Gamma,sigweight);
+                h1_Mass4Gamma_BG->Fill(Mass4Gamma,bkgweight);
+                h1_MassPi0Gamma1_Signal->Fill(MassPi0Gamma1,sigweight);
+                h1_MassPi0Gamma2_Signal->Fill(MassPi0Gamma2,sigweight);
+                h1_MassPi0Gamma1_BG->Fill(MassPi0Gamma1,bkgweight);
+                h1_MassPi0Gamma2_BG->Fill(MassPi0Gamma2,bkgweight);
+            }
+        }
+    }
+
+    TCanvas *c_Mass2Gamma = new TCanvas("c_Mass2Gamma","c_Mass2Gamma",1600,800);
+    c_Mass2Gamma->Divide(3,2);
+    c_Mass2Gamma->cd(1);
+    h1_MassGamma2Gamma3->GetXaxis()->SetTitle("m(#gamma_{2}#gamma_{3}) [GeV]");
+    h1_MassGamma2Gamma3->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassGamma2Gamma3->GetBinWidth(1)));
+    h1_MassGamma2Gamma3->Draw();
+    c_Mass2Gamma->cd(2);
+    h1_MassGamma2Gamma4->GetXaxis()->SetTitle("m(#gamma_{2}#gamma_{4}) [GeV]");
+    h1_MassGamma2Gamma4->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassGamma2Gamma4->GetBinWidth(1)));
+    h1_MassGamma2Gamma4->Draw();
+    c_Mass2Gamma->cd(3);
+    h1_MassGamma2Gamma5->GetXaxis()->SetTitle("m(#gamma_{2}#gamma_{5}) [GeV]");
+    h1_MassGamma2Gamma5->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassGamma2Gamma5->GetBinWidth(1)));
+    h1_MassGamma2Gamma5->Draw();
+    c_Mass2Gamma->cd(4);
+    h1_MassGamma3Gamma4->GetXaxis()->SetTitle("m(#gamma_{3}#gamma_{4}) [GeV]");
+    h1_MassGamma3Gamma4->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassGamma3Gamma4->GetBinWidth(1)));
+    h1_MassGamma3Gamma4->Draw();
+    c_Mass2Gamma->cd(5);
+    h1_MassGamma3Gamma5->GetXaxis()->SetTitle("m(#gamma_{3}#gamma_{5}) [GeV]");
+    h1_MassGamma3Gamma5->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassGamma3Gamma5->GetBinWidth(1)));
+    h1_MassGamma3Gamma5->Draw();
+    c_Mass2Gamma->cd(6);
+    h1_MassGamma4Gamma5->GetXaxis()->SetTitle("m(#gamma_{4}#gamma_{5}) [GeV]");
+    h1_MassGamma4Gamma5->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassGamma4Gamma5->GetBinWidth(1)));
+    h1_MassGamma4Gamma5->Draw();
+    c_Mass2Gamma->SaveAs(Form("%sMass2Gamma_SG_hist.pdf",outDir.Data()));
+
+    TCanvas *c_Mass4Gamma = new TCanvas("c_Mass4Gamma","c_Mass4Gamma",1000,800);
+    c_Mass4Gamma->Divide(1,2);
+    c_Mass4Gamma->cd(1);
+    h1_Mass4Gamma_Signal->GetXaxis()->SetTitle("m_{4#gamma} [GeV])");
+    h1_Mass4Gamma_Signal->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_Mass4Gamma_Signal->GetBinWidth(1)));
+    h1_Mass4Gamma_Signal->SetTitle("sWeighted m_{4#gamma} Signal");
+    h1_Mass4Gamma_Signal->Draw();
+    c_Mass4Gamma->cd(2);
+    h1_Mass4Gamma_BG->GetXaxis()->SetTitle("m_{4#gamma} [GeV])");
+    h1_Mass4Gamma_BG->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_Mass4Gamma_BG->GetBinWidth(1)));
+    h1_Mass4Gamma_BG->SetTitle("sWeighted m_{4#gamma} Background");
+    h1_Mass4Gamma_BG->Draw();
+    c_Mass4Gamma->SaveAs(Form("%sMass4Gamma_SG_BG_hist.pdf",outDir.Data()));
+
+    TCanvas *c_MassPi0Gamma1 = new TCanvas("c_MassPi0Gamma1","c_MassPi0Gamma1",1000,800);
+    c_MassPi0Gamma1->Divide(1,2);
+    c_MassPi0Gamma1->cd(1);
+    h1_MassPi0Gamma1_Signal->GetXaxis()->SetTitle("m(#pi^{0}#gamma_{2}) [GeV])");
+    h1_MassPi0Gamma1_Signal->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassPi0Gamma1_Signal->GetBinWidth(1)));
+    h1_MassPi0Gamma1_Signal->SetTitle("sWeighted m(#pi^{0}#gamma_{2}) Signal");
+    h1_MassPi0Gamma1_Signal->Draw();
+    c_MassPi0Gamma1->cd(2);
+    h1_MassPi0Gamma1_BG->GetXaxis()->SetTitle("m(#pi^{0}#gamma_{2}) [GeV])");
+    h1_MassPi0Gamma1_BG->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassPi0Gamma1_BG->GetBinWidth(1)));
+    h1_MassPi0Gamma1_BG->SetTitle("sWeighted m(#pi^{0}#gamma_{2}) Background");
+    h1_MassPi0Gamma1_BG->Draw();
+    c_MassPi0Gamma1->SaveAs(Form("%sMassPi0Gamma1_SG_BG_hist.pdf",outDir.Data()));
+
+    TCanvas *c_MassPi0Gamma2 = new TCanvas("c_MassPi0Gamma2","c_MassPi0Gamma2",1000,800);
+    c_MassPi0Gamma2->Divide(1,2);
+    c_MassPi0Gamma2->cd(1);
+    h1_MassPi0Gamma2_Signal->GetXaxis()->SetTitle("m(#pi^{0}#gamma_{3}) [GeV])");
+    h1_MassPi0Gamma2_Signal->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassPi0Gamma2_Signal->GetBinWidth(1)));
+    h1_MassPi0Gamma2_Signal->SetTitle("sWeighted m(#pi^{0}#gamma_{3}) Signal");
+    h1_MassPi0Gamma2_Signal->Draw();
+    c_MassPi0Gamma2->cd(2);
+    h1_MassPi0Gamma2_BG->GetXaxis()->SetTitle("m(#pi^{0}#gamma_{3}) [GeV])");
+    h1_MassPi0Gamma2_BG->GetYaxis()->SetTitle(Form("Events/%.3f GeV",h1_MassPi0Gamma2_BG->GetBinWidth(1)));
+    h1_MassPi0Gamma2_BG->SetTitle("sWeighted m(#pi^{0}#gamma_{3}) Background");
+    h1_MassPi0Gamma2_BG->Draw();
+    c_MassPi0Gamma2->SaveAs(Form("%sMassPi0Gamma2_SG_BG_hist.pdf",outDir.Data()));
+
+    // delete all objects on the heap
+    delete h1_MassGamma2Gamma3;
+    delete h1_MassGamma2Gamma4;
+    delete h1_MassGamma2Gamma5;
+    delete h1_MassGamma3Gamma4;
+    delete h1_MassGamma3Gamma5;
+    delete h1_MassGamma4Gamma5;
+    delete h1_Mass4Gamma_Signal;
+    delete h1_Mass4Gamma_BG;
+    delete h1_MassPi0Gamma1_Signal;
+    delete h1_MassPi0Gamma2_Signal;
+    delete h1_MassPi0Gamma1_BG;
+    delete h1_MassPi0Gamma2_BG;
+    delete c_Mass2Gamma;
+    delete c_Mass4Gamma;
+    delete c_MassPi0Gamma1;
+    delete c_MassPi0Gamma2;
+    // delete wts[NMass4GammaBins];    
 }
 
 void my_style(){
